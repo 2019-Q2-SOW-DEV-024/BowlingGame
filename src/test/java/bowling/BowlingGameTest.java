32fc18fc923d97ec90f1ec22f6c1acb7d5f6f14b
2019-Q2-SOW-DEV-024 <pavanicrazy123@gmail.com>
2019-06-11 12:10:48 +0200

    validation of set up with test class

---
+++ src/test/java/bowling/BowlingGameTest.java
@@ -0,0 +1,12 @@
+package bowling;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+	@Test
+	public void validateSetUp() {
+		assertTrue(true);
+	}
+}
